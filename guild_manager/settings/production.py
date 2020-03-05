83b7f615030eb642b6382a416db54630b302b51b
Rob Lynch <roblynch@protonmail.com>
2020-03-04 23:29:58 -0500

    Update order

--- guild_manager/settings/production.py
+++ guild_manager/settings/production.py
@@ -1,11 +1,13 @@
 from guild_manager.settings.base import *
+
 import django_heroku
+
+key = os.environ.get('SECRET_KEY')
+SECRET_KEY = key
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
-key = os.environ.get('SECRET_KEY')
-SECRET_KEY = key
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
