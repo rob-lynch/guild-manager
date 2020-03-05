a8e624d4d63154c77a18eb544d4be97806a82091
Rob Lynch <roblynch@protonmail.com>
2020-03-04 23:58:37 -0500

    Use env var

--- guild_manager/settings/production.py
+++ guild_manager/settings/production.py
@@ -1,12 +1,12 @@
 from guild_manager.settings.base import *
-from django.utils.crypto import get_random_string
+
 import django_heroku
 
-chars='abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-SECRET_KEY=get_random_string(50, chars)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
