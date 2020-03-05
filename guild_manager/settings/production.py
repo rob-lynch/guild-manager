d58c22eb0489a7f12d6edd415b0af15b670ed342
Rob Lynch <roblynch@protonmail.com>
2020-03-04 23:49:57 -0500

    Remove debug

--- guild_manager/settings/production.py
+++ guild_manager/settings/production.py
@@ -5,11 +5,8 @@ import django_heroku
 chars='abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 SECRET_KEY=get_random_string(50, chars)
 
-DEBUG = True
-
 # Activate Django-Heroku.
 django_heroku.settings(locals())
-
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
