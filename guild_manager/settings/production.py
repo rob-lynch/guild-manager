0cd7c46dba853def60d3386ef7ba0aa667945225
Rob Lynch <roblynch@protonmail.com>
2020-03-05 00:24:53 -0500

    Heroku updates (#2)
    * Clarify access required for OAUTH
    
    * Get/Set SECRET_KEY for Heroku
    
    * Correct name of Heroku settings file
    
    * Update default DJANGO_SETTINGS_MODULE value

--- guild_manager/settings/production.py
+++ guild_manager/settings/production.py
@@ -1,8 +1,12 @@
 from guild_manager.settings.base import *
+
 import django_heroku
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
