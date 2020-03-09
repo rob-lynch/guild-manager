12ece79b670d985acead98a6cf12fac0e97d1eed
Rob Lynch <roblynch@protonmail.com>
2020-03-09 16:13:55 -0400

    Add test

--- guild_management/tests.py
+++ guild_management/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from guild_management.models import Realm
 
 # Create your tests here.
+
+
+class RealmTestCase(TestCase):
+    def setUp(self):
+        Realm.objects.create(name="TestRealm")
+
+    def test_realm_is_created(self):
+        """The Realm that is created returns the expected name"""
+        realm = Realm.objects.get(name="TestRealm")
+        self.assertEqual(realm.name, 'TestRealm')
