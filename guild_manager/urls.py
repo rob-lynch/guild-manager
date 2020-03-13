1f9328f0ce3145efa60324a6293da1df4a52f67a
Rob Lynch <roblynch@protonmail.com>
2020-03-12 23:42:42 -0400

    Allow for a Dynamic Header/Title

--- guild_manager/urls.py
+++ guild_manager/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import url
 from .views import redirect_view
+import os
 
 urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
@@ -10,5 +11,12 @@ urlpatterns = [
     path('', redirect_view),
 ]
 
-admin.site.site_header = 'Guild Manager'
-admin.site.site_title = 'Guild Manager'
+if 'SITE_HEADER' in os.environ:
+    admin.site.site_header = os.environ['SITE_HEADER']
+else:
+    admin.site.site_header = "Guild Manager"
+
+if 'SITE_TITLE' in os.environ:
+    admin.site.site_title =  os.environ['SITE_TITLE']
+else:
+    admin.site.site_title = "Guild Manager"
